ed5285a795a9b1e2909b2c9b34fffacf714e9c0a
emanuela <emanuela.markovic@hotmail.com>
2024-04-13 09:34:23 +0200

    Add CORS support and allow requests from specific frontend URLs

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import "./config.js";
+import cors from "cors";
 import endpointsInfosHTMLResponse from "./utils/endPointsInfosHTMLResponse.js";
 import { todoRouter, todosMainPath } from "./routes/todoRoutes.js";
 
@@ -8,6 +9,19 @@ const port = process.env.PORT;
 const app = express();
 
 app.use(express.json());
+
+const allowedOrigins = ["http://localhost:8888", "http://localhost:3000"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 app.use(todosMainPath, todoRouter);
 app.get("/", endpointsInfosHTMLResponse);
