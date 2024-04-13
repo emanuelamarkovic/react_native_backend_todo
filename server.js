497a18e1ad975dce22efad8f0fa4f23701572b6e
emanuela <emanuela.markovic@hotmail.com>
2024-04-13 08:54:06 +0200

    create basic express js structure and add utils folder

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import "./config.js";
+import logEndpoints from "./utils/logEndPoints.js";
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port} \n`);
+  logEndpoints(app, port);
+});
